f230fdee8bdfd160ed3cf2f1a388aeda0d55bf60
weo <413122031@qq.com>
2018-05-05 15:50:55 +0800

    add user_puid

--- robot.py
+++ robot.py
@@ -3,7 +3,7 @@ from wxpy import *
 
 import requests
 bot = Bot()
-
+bot.enable_puid('wxpy_puid.pkl')
 
 # 自动接受新的好友请求
 @bot.register(msg_types=FRIENDS)
@@ -11,17 +11,21 @@ def auto_accept_friends(msg):
     # 接受好友请求
     new_friend = msg.card.accept()
     # 向新的好友发送消息
-    new_friend.send('哈哈，我自动接受了你的好友请求')
+    new_friend.send('您好，需要什么电影可以直接发送电影名字哦')
 
-@bot.register()
+@bot.register(msg_types=TEXT)
 def print_others(msg):
     print(msg)
     data = str(msg).split(" : ")
     user_name = data[0]
     print (user_name)
-    text = data[1].split(" (Text)")[0]
+    user_movie = data[1].split(" (Text)")[0]
     my_friend = bot.friends().search(user_name)[0]
-    my_friend.send(text)
+    user_piud = my_friend.puid
+    res = "http://0.0.0.0:8080/session?uid={}&name={}&movie={}".format(
+        user_piud,user_name,user_movie
+    )
+    my_friend.send(res)
 
 
 embed()
